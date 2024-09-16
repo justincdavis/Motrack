401cee9949a626398bded8f6eb9a2b44cde8694b
justincdavis <davis.justin@mssm.org>
2024-09-16 16:59:57 -0500

    Switch to using pyproject.toml instead of setup.py, fix top level import

---
+++ motrack/__init__.py
@@ -0,0 +1,31 @@
+from __future__ import annotations
+
+from . import (
+    cmc,
+    common,
+    config_parser,
+    datasets,
+    evaluation,
+    filter,
+    library,
+    object_detection,
+    reid,
+    tools,
+    tracker,
+    utils,
+)
+
+__all__ = [
+    "cmc",
+    "common",
+    "config_parser",
+    "datasets",
+    "evaluation",
+    "filter",
+    "library",
+    "object_detection",
+    "reid",
+    "tools",
+    "tracker",
+    "utils",
+]
