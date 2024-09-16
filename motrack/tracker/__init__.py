59112ca598d3aa34e22a681a73ebd7d78005dba9
justincdavis <davis.justin@mssm.org>
2024-09-16 17:20:41 -0500

    Update motrack/tracker/init to support provided examples

--- motrack/tracker/__init__.py
+++ motrack/tracker/__init__.py
@@ -10,3 +10,16 @@ from motrack.tracker.trackers.algorithms.byte import ByteTracker
 from motrack.tracker.trackers.algorithms.sort import SortTracker
 from motrack.tracker.trackers.algorithms.sparse import SparseTracker
 from motrack.tracker.trackers.factory import tracker_factory
+from motrack.tracker.tracklet import Tracklet, TrackletState
+
+__all__ = [
+    'Tracker',
+    'ByteTracker',
+    'SortTracker',
+    'SparseTracker',
+    'tracker_factory',
+    'Tracklet',
+    'TrackletState',
+    'AssociationAlgorithm',
+    'IoUAssociation',
+]
